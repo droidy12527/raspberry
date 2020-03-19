69c28f147aa349fbcb77e3c0b0810b8992792d09
droidprogrammmer <53708038+droidy12527@users.noreply.github.com>
2020-03-19 22:22:07 +0530

    Update prathamesh_new.py

--- prathamesh_new.py
+++ prathamesh_new.py
@@ -19,14 +19,13 @@ GPIO.setup(BUZZER, GPIO.OUT)
 
 def upload_cloud(data):
 	config = {
-	    "apiKey": "AIzaSyC-qt2eSGN9MolkfLHhU8DKmLXFPnMpuNg",
-	    "authDomain": "intruderdetector-a5506.firebaseapp.com",
-	    "databaseURL": "https://intruderdetector-a5506.firebaseio.com",
-	    "projectId": "intruderdetector-a5506",
-	    "storageBucket": "intruderdetector-a5506.appspot.com",
-	    "messagingSenderId": "136524713787",
-	    "appId": "1:136524713787:web:a82bfb6f69f09e5d37b11e",
-	    "measurementId": "G-Y5GXZ7BVHV"
+	    "apiKey": "AIzaSyCsRnBbJru3Jg5crfEkJnpvFvSlzWJatWc",
+    	"authDomain": "intuderdetector.firebaseapp.com",
+    "databaseURL": "https://intuderdetector.firebaseio.com",
+    "projectId": "intuderdetector",
+    "storageBucket": "intuderdetector.appspot.com",
+    "messagingSenderId": "834194595303",
+    "appId": "1:834194595303:web:d5bcdd3c6de997f7f7f142
 	}
 	folder_name = time.strftime("%d-%m-%Y")
 	file_name = data
